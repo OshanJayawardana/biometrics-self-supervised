6455230329a29364c382efb202ae622800a6363b
Oshan Jayawardena <62610397+OshanJayawardana@users.noreply.github.com>
2022-05-02 22:52:44 +0530

    "gait initial codes added"

---
+++ experiments/Gait/scen.1/supervised/trainers.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import tensorflow_addons as tfa
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras import layers
+from sklearn.manifold import TSNE
+from sklearn.metrics import roc_curve
+
+from backbones import *
+from data_loader import *
+
+def trainer(samples_per_user):
+  frame_size   = 30
+  path = "/home/oshanjayawardanav100/biometrics-self-supervised/musicid_dataset/"
+  
+  users_2 = list(range(9,21)) #Users for dataset 1
+  users_1 = users = list(range(1,7)) #Users for dataset 2
+  folder_train = ["TrainingSet"]
+  folder_val = ["TestingSet"]
+  folder_test = ["TestingSet_secret"]
+  
+  x_train, y_train, sessions_train = data_load_origin(path, users=users_1, folders=folder_train, frame_size=30)
+  print("training samples : ", x_train.shape[0])
+  
+  x_val, y_val, sessions_val = data_load_origin(path, users=users_1, folders=folder_val, frame_size=30)
+  print("validation samples : ", x_val.shape[0])
+  
+  x_test, y_test, sessions_test = data_load_origin(path, users=users_1, folders=folder_test, frame_size=30)
+  print("testing samples : ", x_test.shape[0])
+  
+  classes, counts  = np.unique(y_train, return_counts=True)
+  num_classes = len(classes)
+  print("minimum samples per user : ", min(counts)) #60
+  
+  x_train, x_val, x_test = norma(x_train, x_val, x_test)
+  print("x_train", x_train.shape)
+  print("x_val", x_val.shape)
+  print("x_test", x_test.shape)
+  x_all_tsne = np.concatenate((x_train, x_val, x_test), axis=0)
+  y_all_tsne = np.concatenate((y_train, y_val, y_test), axis=0)
+  
+  x_train, y_train = user_data_split(x_train ,y_train , samples_per_user=samples_per_user)
+  print("limited training samples : ", x_train.shape[0])
+  classes, counts  = np.unique(y_train, return_counts=True)
+  print(counts)
+  
+  ks = 3
+  con =3
+  inputs = Input(shape=(frame_size, x_train.shape[-1]))
+  x = Conv1D(filters=16*con,kernel_size=ks,strides=1, padding='same')(inputs) 
+  x = BatchNormalization()(x)
+  x = ReLU()(x)
+  x = MaxPooling1D(pool_size=4, strides=4)(x)
+  x = Dropout(rate=0.1)(x)
+  x = resnetblock_final(x, CR=32*con, KS=ks)
+  x = Flatten()(x)
+  x = Dense(256, activation='relu')(x)
+  x = Dense(64, activation='relu')(x)
+  outputs = Dense(num_classes, activation='softmax')(x)
+  resnettssd = Model(inputs, outputs)
+  #resnettssd.summary()
+  
+  callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', restore_best_weights=True, patience=5)
+  lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate = 0.001, decay_rate=0.95, decay_steps=1000000)# 0.0001, 0.9, 100000
+  optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+  #optimizer = tf.keras.optimizers.Adam()
+  resnettssd.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'] )
+  history = resnettssd.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=100, callbacks=callback, batch_size=8)
+  
+  results = resnettssd.evaluate(x_test,y_test)
+  test_acc = results[1]
+  print("test acc:", results[1])
+  
+  #Calculating kappa score
+  metric = tfa.metrics.CohenKappa(num_classes=num_classes, sparse_labels=True)
+  metric.update_state(y_true=y_test , y_pred=resnettssd.predict(x_test))
+  result = metric.result()
+  kappa_score = result.numpy()
+  print('kappa score: ',result.numpy())
+  
+  return test_acc, kappa_score
---
+++ experiments/Gait/scen.3/transfer/plot_results.py
@@ -0,0 +1,65 @@
+from trainers import *
+from pre_trainers import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plotspu(ft):
+  fet_extrct = pre_trainer()
+  # single layer: ft=5
+  # 2 layer: ft=8
+  # 3 layer: ft=11
+  # 4 layer: ft=12
+  # all layer: ft=17
+  
+  if ft==0:
+    model_name="musicid_scen3_transfer"
+  else:
+    model_name='musicid_scen3'+'_ft'+str(ft)+'_transfer'
+  
+  variable_name="sampes per user"
+  variable=[1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,24,28,32,36,40,45,50,55,60]
+  acc=[]
+  kappa=[]
+  for el in variable:
+    acc_temp=[]
+    kappa_temp=[]
+    for itr in range(10):
+      test_acc, kappa_score = trainer(el, fet_extrct, ft=ft) #Edit this line for trainer parameters
+      acc_temp.append(test_acc)
+      kappa_temp.append(kappa_score)
+    acc.append(acc_temp)
+    kappa.append(kappa_temp)
+  acc = np.array(acc)
+  kappa = np.array(kappa)
+  
+  np.savez("graph_data/"+model_name+".npz", test_acc=acc, kappa_score=kappa)
+  print(acc.shape)
+  print(kappa.shape)
+  
+  kappa_max = np.max(kappa, axis=1)
+  plt.figure(figsize=(12,8))
+  plt.plot(variable,kappa_max, 'm', label=model_name)
+  plt.title("kappa score vs "+variable_name)
+  plt.xlabel(variable_name)
+  plt.ylabel("kappa score")
+  plt.legend()
+  plt.show()
+  if ft==0:
+    plt.savefig('graphs/kappa.jpg')
+  else:
+    plt.savefig('graphs/kappa'+'_ft'+str(ft)+'.jpg')
+  plt.close()
+  
+  acc_max = np.max(acc, axis=1)
+  plt.figure(figsize=(12,8))
+  plt.plot(variable,acc_max, 'm', label=model_name)
+  plt.title("test accuracy vs "+variable_name)
+  plt.xlabel(variable_name)
+  plt.ylabel("test acuracy")
+  plt.legend()
+  plt.show()
+  if ft==0:
+    plt.savefig('graphs/acc.jpg')
+  else:
+    plt.savefig('graphs/acc'+'_ft'+str(ft)+'.jpg')
+  plt.close()
