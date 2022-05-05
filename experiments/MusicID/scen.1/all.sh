d13cb66ff1f714a7e1cdf18d11b0300a1410beab
OshanJayawardana <180437u@uom.lk>
2022-05-05 04:51:53 +0000

    musicid scen 3 and 1 users corrected to 14

--- experiments/MusicID/scen.1/all.sh
+++ experiments/MusicID/scen.1/all.sh
@@ -2,4 +2,12 @@ cd supervised ||
 python3 plot_results.py ||
 cd .. ||
 cd da ||
-python3 plot_results.py
+python3 plot_results.py ||
+cd .. ||
+cd multi_task ||
+python3 plot_loop.py ||
+cd .. ||
+cd simsiam ||
+python3 plot_loop.py ||
+cd .. ||
+python3 plots.py
